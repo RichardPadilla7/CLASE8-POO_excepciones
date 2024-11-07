e8eca78160c8a5cd1d529158a9c1531f84cb09c9
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-11-07 08:10:06 -0500

    RICHARD

---
+++ src/EXCEPCIONES/Main.java
@@ -0,0 +1,25 @@
+// EXCEPCIONES
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        String nombre;
+        Scanner ingresar = new Scanner(System.in);
+
+        System.out.println("Ingrese un nombre: ");
+        nombre = ingresar.nextLine();
+
+        System.out.println("El nombre a imprimir se multiplicara por 2; " + nombre*2);
+
+
+
+
+
+
+
+
+
+
+    }
+}
