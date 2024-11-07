838305ba6e03b453dfb9b159188d697159a91e49
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-11-07 08:10:23 -0500

    1234

--- src/EXCEPCIONES/Main.java
+++ src/EXCEPCIONES/Main.java
@@ -1,21 +1,48 @@
-// EXCEPCIONES
+package EXCEPCIONES;// EXCEPCIONES
 
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String nombre;
-        Scanner ingresar = new Scanner(System.in);
+        /*
+        // Convertir de string a entero
 
-        System.out.println("Ingrese un nombre: ");
-        nombre = ingresar.nextLine();
+        String val1 = "22";
+        int convertir;
 
-        System.out.println("El nombre a imprimir se multiplicara por 2; " + nombre*2);
+        // Como transformar un string a un entero si necesidad de cambiar el tipo del atributo
+        convertir = Integer. parseInt(val1);
+        System.out.println("El valor a multiplicar sera por 2; " + convertir * 2);
+        */
 
 
+        /*
+        // Convertir de string a double
+
+        String val1 = "22.5";
+        double convertir;
+
+        // Como transformar un string a un entero si necesidad de cambiar el tipo del atributo
+        convertir = Double.parseDouble(val1);
+        System.out.println("El valor a multiplicar sera por 2; " + convertir * 2);
+        */
 
 
+        /*
+        //Convertir de double a entero
+        double n1 = 2.1;
+        int convertir;
 
+        convertir = (int) n1;
+        System.out.println("El numero a imprimir es; " + convertir);
+        */
+
+        // convertir de entero a cadena(String)
+        int n1 = 2;
+        String convertir;
+
+        convertir = String.valueOf(n1);
+        System.out.println("El numero a imprimir a cadena es: " + convertir);
 
 
 
